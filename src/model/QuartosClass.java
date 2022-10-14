e247b494cedb1cdf4289664d810a3f013213d047
Vinicius Kalach <viniciuscato@hotmail.com>
2022-10-14 18:29:16 -0300

    FEAT - criar classes de quarto e categoria

---
+++ src/model/QuartosClass.java
@@ -0,0 +1,41 @@
+
+package Classes;
+
+public class QuartosClass {
+    private int número;
+    private int andar;
+    private boolean ocupado;
+    private int categora_id;
+
+    public int getNúmero() {
+        return número;
+    }
+
+    public void setNúmero(int número) {
+        this.número = número;
+    }
+
+    public int getAndar() {
+        return andar;
+    }
+
+    public void setAndar(int andar) {
+        this.andar = andar;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    public int getCategora_id() {
+        return categora_id;
+    }
+
+    public void setCategora_id(int categora_id) {
+        this.categora_id = categora_id;
+    }
+}
