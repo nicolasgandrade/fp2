498a6f19245cb6b141b63f37b035e0390265f6c5
nicolasgandrade <nicolasguerrero72@gmail.com>
2022-10-17 21:07:54 -0300

    FIX - Muda nome das variaveis

--- src/view/Login.java
+++ src/view/Login.java
@@ -11,56 +11,56 @@ public class Login extends javax.swing.JFrame {
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
+        panelLogin = new javax.swing.JPanel();
+        lblUsername = new javax.swing.JLabel();
+        txtName = new javax.swing.JTextField();
+        lblSenha = new javax.swing.JLabel();
+        txtSenha = new javax.swing.JPasswordField();
+        btnEntrar = new javax.swing.JButton();
         Fundo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(new java.awt.Color(226, 224, 214));
-        jPanel1.setForeground(new java.awt.Color(226, 224, 214));
-        jPanel1.setLayout(new java.awt.GridBagLayout());
+        panelLogin.setBackground(new java.awt.Color(226, 224, 214));
+        panelLogin.setForeground(new java.awt.Color(226, 224, 214));
+        panelLogin.setLayout(new java.awt.GridBagLayout());
 
-        jLabel1.setText("Nome de usuário");
+        lblUsername.setText("Nome de usuário");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.insets = new java.awt.Insets(9, 34, 9, 34);
-        jPanel1.add(jLabel1, gridBagConstraints);
+        panelLogin.add(lblUsername, gridBagConstraints);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weightx = 0.2;
         gridBagConstraints.insets = new java.awt.Insets(9, 34, 9, 34);
-        jPanel1.add(jTextField1, gridBagConstraints);
+        panelLogin.add(txtName, gridBagConstraints);
 
-        jLabel2.setText("Senha");
+        lblSenha.setText("Senha");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.insets = new java.awt.Insets(9, 34, 9, 34);
-        jPanel1.add(jLabel2, gridBagConstraints);
+        panelLogin.add(lblSenha, gridBagConstraints);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weightx = 0.2;
         gridBagConstraints.insets = new java.awt.Insets(9, 34, 9, 34);
-        jPanel1.add(jPasswordField1, gridBagConstraints);
+        panelLogin.add(txtSenha, gridBagConstraints);
 
-        jButton1.setText("Entrar");
+        btnEntrar.setText("Entrar");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.insets = new java.awt.Insets(9, 34, 9, 34);
-        jPanel1.add(jButton1, gridBagConstraints);
+        panelLogin.add(btnEntrar, gridBagConstraints);
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 220, 400, 280));
+        getContentPane().add(panelLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 220, 400, 280));
 
         Fundo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/Fundo_icon.png"))); // NOI18N
         getContentPane().add(Fundo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -79,11 +79,11 @@ public class Login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Fundo;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton btnEntrar;
+    private javax.swing.JLabel lblSenha;
+    private javax.swing.JLabel lblUsername;
+    private javax.swing.JPanel panelLogin;
+    private javax.swing.JTextField txtName;
+    private javax.swing.JPasswordField txtSenha;
     // End of variables declaration//GEN-END:variables
 }
