f5eb91fcbd1306c4fc48d860808150756d2d0660
Felipe Sanches Franqueira <felipe04sfr@outlook.com>
2022-10-21 15:47:09 -0300

    REFACTOR - Renomeia classe Hospede e remove linhas extras

--- src/model/Hospede.java
+++ src/model/Hospede.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Luiz
- */
-public class Hospedes {
+public class Hospede {
+    
     private int id;
     private String nome;
     private String telefone;
