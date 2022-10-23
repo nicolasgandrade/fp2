c351d139f3bc07856e388de74811230657fea146
Vinicius Kalach <viniciuscato@hotmail.com>
2022-10-22 22:32:35 -0300

    FIX - Corrigir o nome

--- src/view/ViewSideNav.java
+++ src/view/ViewSideNav.java
@@ -1,11 +1,11 @@
 package view;
 
-public class ViewSlideNav extends javax.swing.JFrame {
+public class ViewSideNav extends javax.swing.JFrame {
 
     /**
      * Creates new form ViewSlideNav
      */
-    public ViewSlideNav() {
+    public ViewSideNav() {
         initComponents();
         
         jpHospedes.setVisible(false);
@@ -326,20 +326,21 @@ public class ViewSlideNav extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ViewSlideNav.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewSideNav.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ViewSlideNav.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewSideNav.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ViewSlideNav.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewSideNav.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ViewSlideNav.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewSideNav.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ViewSlideNav().setVisible(true);
+                new ViewSideNav().setVisible(true);
             }
         });
     }
