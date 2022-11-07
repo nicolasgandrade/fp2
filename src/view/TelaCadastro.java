32efc04ae9679b987da77a0d9bb2ecc0eca2aa94
Lucas Camargo <camargolucasd@gmail.com>
2022-11-07 19:33:11 -0300

    FIX - Corrige numero de celular

--- src/view/TelaCadastro.java
+++ src/view/TelaCadastro.java
@@ -35,7 +35,7 @@ public class TelaCadastro extends javax.swing.JFrame {
         lblDocumento.setText("Documento");
 
         try {
-            txtTelefone.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("+## ##-########")));
+            txtTelefone.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("+## (##)  #####-####")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
